7095a113b4e5506e9850b138bb7914d66f57bb11
Franck Noubissi <emrickfranck@gmail.com>
2021-10-30 17:24:57 +0200

    Environnement prêt

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
+
+//Automatic loading
+mix.browserSync("http://crm.test");
